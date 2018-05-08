244e357ebea485fc7caf33fe7cea5318f4125e3b
aaric <vipaaric@gmail.com>
2018-05-08 16:25:21 +0800

    0.0.1-SNAPSHOT init

---
+++ src/test/java/com/github/aaric/achieve/kotlin/KotlinTest.java
@@ -0,0 +1,17 @@
+package com.github.aaric.achieve.kotlin;
+
+import org.junit.Test;
+
+/**
+ * KotlinTest
+ *
+ * @author Aaric, created on 2018-05-08T16:20.
+ * @since 0.0.1-SNAPSHOT
+ */
+public class KotlinTest {
+
+    @Test
+    public void testPrint() {
+        System.out.println("hello");
+    }
+}
